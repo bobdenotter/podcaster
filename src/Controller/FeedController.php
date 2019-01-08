b3bde33051e2efa4693c5e9d377d364f492319bf
Bob den Otter <bob@twokings.nl>
2019-01-08 16:38:15 +0100

    Update FeedController.php

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use getID3;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +13,14 @@ class FeedController
     /**
      * @Route("/feed/{slug}")
      */
-    public function feed(string $slug): Response
+    public function feed(string $slug, Request $request): Response
     {
         $finder = new Finder();
         $finder->files()->in(__DIR__ . '/../../public/files/kkc-01')->name('*.mp3');
 
-        $baseUrl = "https://nu.nl";
+        $baseUrl = 'http://' . $request->server->get('HTTP_HOST') . '/files/kkc-01';
+
+//        dd($request->server->get('HTTP_HOST'));
 
         $feed = new \Castanet_Feed('Title', 'https://nos.nl', 'Een boek');
         $getID3 = new getID3;
@@ -29,12 +32,13 @@ class FeedController
             $info = $getID3->analyze($file->getRealPath());
 
             $url = $baseUrl . "/" . $file->getRelativePathname();
-//            dump($info);
+            dump($info);
 
             $item->setTitle($info['id3v1']['title'] ? $info['id3v1']['title'] : $info['filename']);
             $item->setLink($url);
             $item->setMediaUrl($url);
-
+            $item->setMediaMimeType('audio/mpeg');
+            $item->setMediaSize($info['filesize']);
 
             $feed->addItem($item);
             // dumps the absolute path
@@ -50,9 +54,10 @@ class FeedController
         // this looks exactly the same
 //        return new Response((string) $feed);
 
-        dd((string) $feed);
+//        dd((string) $feed);
 
 
+        return new Response((string) $feed);
 
         return new Response("<html><body>foo $slug</body></html>");
 
